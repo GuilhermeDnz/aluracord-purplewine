ff4fdbabdc924b39953d2054745d42b17b18a8a2
GuilhermeDnz <guilhermednz@outlook.com.br>
2022-01-30 14:02:36 -0300

    Alterações visual chat

--- pages/chat.js
+++ pages/chat.js
@@ -64,8 +64,8 @@ export default function ChatPage() {
                     flexDirection: 'column',
                     flex: 1,
                     boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                    borderRadius: '5px',
-                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                    borderRadius: '20px',
+                    backgroundColor: "rgba(28, 14, 48, 0.4)",
                     height: '100%',
                     maxWidth: '95%',
                     maxHeight: '95vh',
@@ -79,9 +79,9 @@ export default function ChatPage() {
                         display: 'flex',
                         flex: 1,
                         height: '80%',
-                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        backgroundColor: "rgba(0, 0, 0, 0.9)",
                         flexDirection: 'column',
-                        borderRadius: '5px',
+                        borderRadius: '20px',
                         padding: '16px',
                     }}
                 >
@@ -120,7 +120,7 @@ export default function ChatPage() {
                                 resize: 'none',
                                 borderRadius: '5px',
                                 padding: '6px 8px',
-                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                backgroundColor: "rgba(18, 18, 18, 0.3)",
                                 marginRight: '12px',
                                 color: appConfig.theme.colors.neutrals[200],
                             }}
@@ -140,10 +140,26 @@ function Header() {
                     Chat
                 </Text>
                 <Button
-                    variant='tertiary'
-                    colorVariant='neutral'
                     label='Logout'
                     href="/"
+                    
+                    buttonColors={{
+                        contrastColor: appConfig.theme.colors.neutrals["000"],
+                        mainColorLight: appConfig.theme.colors.primary[400],
+                        mainColorStrong: 'rgba(255, 255, 255, 0.3)',
+                    }}
+                    styleSheet={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+                    borderRadius: '5px',
+                    color: 'white',
+                    padding: '15px 32px',
+                    textdecoration: 'none',
+                    transition: '0.6s',
+                    display: 'inline-block',
+                    fontsize: '16px',
+                    }}
+                    
+                    
                 />
             </Box>
         </>
